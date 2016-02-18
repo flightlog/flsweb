aaa4493fdf31430f5f899408ee7b801e536c9fec
Roman Schlegel <roman.schlegel@gmail.com>
2016-02-18 23:42:27 +0100

    upgraded test dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,11 @@ var config = {
 
     module: {
         loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: "babel"},
+            {test: /.*\.js$/, exclude: /node_modules/, loader: "babel-loader",
+                query: {
+                    presets: ['es2015']
+                }
+            },
             {test: /\.css$/, loader: "style!css"},
             {test: /\.json$/, loader: "json"},
             {test: /\.html$/, exclude: /node_modules/, loader: "raw"},
