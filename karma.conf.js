6f9b2b9e6ffa3340d8492de2921faac789699b28
Roman Schlegel <roman.schlegel@gmail.com>
2016-02-18 23:03:19 +0100

    switched to node 0.12 for ci build, disabled coverage report

--- karma.conf.js
+++ karma.conf.js
@@ -24,30 +24,6 @@ module.exports = function (config) {
 
   if (TARGET === 'PROD') {
     configObject.browsers = ['PhantomJS'];
-    var instanbulInstrumenterLoader = {
-      test: /\.js/,
-      loader: 'istanbul-instrumenter',
-      exclude: /(node_modules|vendor|\.spec\.js)/
-    };
-
-    if (webpackConfig.module.postLoaders) {
-      webpackConfig.module.postLoaders.push(instanbulInstrumenterLoader);
-    } else {
-      webpackConfig.module.postLoaders = [instanbulInstrumenterLoader];
-    }
-
-    configObject.reporters = configObject.reporters.concat(['junit', 'coverage']);
-    configObject.coverageReporter = {
-      dir: '../target/site/cobertura',
-      reporters: [
-        {type: 'html', subdir: 'coverage-html'},
-        {type: 'cobertura', subdir: '.', file: 'coverage.xml'},
-        {type: 'text-summary', subdir: '.', file: 'text-summary.txt'}
-      ]
-    };
-    configObject.junitReporter = {
-      outputDir: '../target/surefire-reports'
-    };
   }
 
   if (TARGET === 'DEV') {
