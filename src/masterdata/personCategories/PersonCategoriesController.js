ad1f87b9cc794d2c78b9f463fc8d45d4e0b6abbe
Roman Schlegel <roman.schlegel@gmail.com>
2017-11-12 16:48:19 +0100

    added edit button to tree nodes

--- src/masterdata/personCategories/PersonCategoriesController.js
+++ src/masterdata/personCategories/PersonCategoriesController.js
@@ -74,20 +74,23 @@ export default class PersonCategoriesController {
         };
 
         $scope.editNode = function (node) {
-            node.CategoryName = window.prompt("Person-Category Name:", node.CategoryName);
+            let CategoryName = window.prompt("Person-Category Name:", node.CategoryName);
 
-            $http.post(GLOBALS.BASE_URL + '/api/v1/personCategories/' + node.PersonCategoryId, Object.assign({}, node, {
-                children: undefined,
-                level: undefined
-            }), {
-                headers: {
-                    'X-HTTP-Method-Override': 'PUT'
-                }
-            })
-                .then(function () {
-                    $route.reload();
+            if(CategoryName) {
+                $http.post(GLOBALS.BASE_URL + '/api/v1/personCategories/' + node.PersonCategoryId, Object.assign({}, node, {
+                    CategoryName: CategoryName,
+                    children: undefined,
+                    level: undefined
+                }), {
+                    headers: {
+                        'X-HTTP-Method-Override': 'PUT'
+                    }
                 })
-                .catch(_.partial(MessageManager.raiseError, 'update', 'person category'));
+                    .then(function () {
+                        $route.reload();
+                    })
+                    .catch(_.partial(MessageManager.raiseError, 'update', 'person category'));
+            }
         };
 
         $scope.deleteNode = function (node) {
