082ef75d0d33673fe416848f4ef4ecd0572c8951
Roman Schlegel <roman.schlegel@gmail.com>
2018-04-01 15:54:12 +0200

    trying to switch to chrome headless (travis ci issue)

--- karma.conf.js
+++ karma.conf.js
@@ -1,34 +1,44 @@
 module.exports = function (config) {
-  var webpackConfig = require('./webpack.config.js');
-  var minimist = require('minimist');
-  var os = require('os');
-  var TARGET = minimist(process.argv.slice(2)).TARGET || 'PROD';
+    var webpackConfig = require('./webpack.config.js');
+    var minimist = require('minimist');
+    var TARGET = minimist(process.argv.slice(2)).TARGET || 'PROD';
 
-  var configObject = {
-    basePath: 'src',
-    frameworks: ['jasmine'],
-    files: ['index.spec.js'],
-    reporters: ['dots'],
-    preprocessors: {
-      'index.spec.js': ['webpack', 'sourcemap']
-    },
-    webpack: {
-      module: webpackConfig.module,
-      resolve: webpackConfig.resolve,
-      devtool: 'inline-source-map'
-    },
-    webpackMiddleware: {
-      noInfo: true
+    var configObject = {
+        basePath: 'src',
+        frameworks: ['jasmine'],
+        files: ['index.spec.js'],
+        reporters: ['dots'],
+        preprocessors: {
+            'index.spec.js': ['webpack', 'sourcemap']
+        },
+        webpack: {
+            module: webpackConfig.module,
+            resolve: webpackConfig.resolve,
+            devtool: 'inline-source-map'
+        },
+        customLaunchers: {
+            FlsChromeHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    ' --remote-debugging-port=9222',
+                ]
+            }
+        },
+        webpackMiddleware: {
+            noInfo: true
+        }
+    };
+
+    if (TARGET === 'PROD') {
+        configObject.browsers = ['FlsChromeHeadless'];
     }
-  };
 
-  if (TARGET === 'PROD') {
-    configObject.browsers = ['PhantomJS'];
-  }
+    if (TARGET === 'DEV') {
+        configObject.browsers = ['Chrome'];
+    }
 
-  if (TARGET === 'DEV') {
-    configObject.browsers = ['Chrome'];
-  }
-
-  config.set(configObject);
+    config.set(configObject);
 };
